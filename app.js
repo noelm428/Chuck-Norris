ded7fdfaa23d0ba0db700f2188ef5a6f26376186
noel rodriguez <noelrodriguez@noels-MacBook.fios-router.home>
2020-12-09 09:36:05 -0500

    final_blow

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-let currentImgIndex = 0;
-let numOfImages = $('.Norris-images').children().length - 1;
+
+  let currentImgIndex = 0;
+  let numOfImages = $('.Norris-images').children().length - 1;
 
 
-//   $(".next").on("click",() => {
-//    $('.Norris-images').children().eq(currentImgIndex).css('display','none')
-//    if (currentImgIndex<numOfImages){
-//       currentImgIndex++;
-//    }
-// else{
-//   currentImgIndex=0
-// }
-//    $('.Norris-images').children().eq(currentImgIndex).css('display','block')
-//
-//   })
+  //   $(".next").on("click",() => {
+  //    $('.Norris-images').children().eq(currentImgIndex).css('display','none')
+  //    if (currentImgIndex<numOfImages){
+  //       currentImgIndex++;
+  //    }
+  // else{
+  //   currentImgIndex=0
+  // }
+  //    $('.Norris-images').children().eq(currentImgIndex).css('display','block')
+  //
+  //   })
 
-$('.previous').on('click', () => {
-  $('.Norris-images').children().eq(currentImgIndex).css('display', 'none')
-  if (currentImgIndex > 0) {
-    currentImgIndex--;
-  } else {
-    currentImgIndex = numOfImages;
-  }
+  $('.previous').on('click', () => {
+    $('.Norris-images').children().eq(currentImgIndex).css('display', 'none')
+    if (currentImgIndex > 0) {
+      currentImgIndex--;
+    } else {
+      currentImgIndex = numOfImages;
+    }
 
-  $('.Norris-images').children().eq(currentImgIndex).css('display', 'block')
-  $.ajax({
-    url: `http:api.icndb.com/jokes/random?exclude=[nerdy,explicit]?escape=javascript`
-  }).then(
-    (data) => {
-      console.log(data);
-      $('#joke').html(data.value.joke)
-    },
-    (error) => {
-      alert('YOUR LINKS BROKEN. *JUDO CHOP*')
-
-
-
-    })
+    $('.Norris-images').children().eq(currentImgIndex).css('display', 'block')
+    $.ajax({
+      url: `http:api.icndb.com/jokes/random?exclude=[nerdy,explicit]?escape=javascripts`
+    }).then(
+      (data) => {
+        console.log(data);
+        $('#joke').html(data.value.joke)
+      },
+      (error) => {
+        alert('YOUR LINKS BROKEN. *JUDO CHOP*')
 
 
-})
+
+      })
+
+
+  })
