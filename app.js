b3c231f46189b3967901a78fbe346b4e44826945
noelm428 <75449468+noelm428@users.noreply.github.com>
2020-12-09 10:03:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,5 @@
-<<<<<<< HEAD
-
-  let currentImgIndex = 0;
-  let numOfImages = $('.Norris-images').children().length - 1;
 
 
-  //   $(".next").on("click",() => {
-  //    $('.Norris-images').children().eq(currentImgIndex).css('display','none')
-  //    if (currentImgIndex<numOfImages){
-  //       currentImgIndex++;
-  //    }
-  // else{
-  //   currentImgIndex=0
-  // }
-  //    $('.Norris-images').children().eq(currentImgIndex).css('display','block')
-  //
-  //   })
-
-  $('.previous').on('click', () => {
-    $('.Norris-images').children().eq(currentImgIndex).css('display', 'none')
-    if (currentImgIndex > 0) {
-      currentImgIndex--;
-    } else {
-      currentImgIndex = numOfImages;
-    }
-
-    $('.Norris-images').children().eq(currentImgIndex).css('display', 'block')
-    $.ajax({
-      url: `http:api.icndb.com/jokes/random?exclude=[nerdy,explicit]?escape=javascripts`
-    }).then(
-      (data) => {
-        console.log(data);
-        $('#joke').html(data.value.joke)
-      },
-      (error) => {
-        alert('YOUR LINKS BROKEN. *JUDO CHOP*')
-
-
-
-      })
-
-
-  })
-=======
 let currentImgIndex = 0;
 let numOfImages = $('.Norris-images').children().length - 1;
 
@@ -83,4 +41,4 @@ $('.previous').on('click', () => {
 
 
 })
->>>>>>> 055892d7d97e8fb6981a9442a39e7ae05578cef4
+
